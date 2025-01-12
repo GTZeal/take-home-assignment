b3715b69b428cf5a17d38c0218974f95fca0128f
Zeeshan Waqar <zeewaqar06@gmail.com>
2025-01-12 18:22:01 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function openNav() {
+    const selfie = document.querySelector('.selfie');
+    const navBar = document.querySelector('.navBar');
+    navBar.style.transform = 'translateX(50%)';
+    selfie.style.transform = 'translateX(-30%)';
+    navBar.style.zIndex = '1';
+}
+
+function closeNav() {
+    const selfie = document.querySelector('.selfie');
+    const navBar = document.querySelector('.navBar');
+    navBar.style.transform = 'translateX(0)';
+    selfie.style.transform = 'translateX(0)';
+    navBar.style.zIndex = '-1';
+}
+
+function pkContent() {
+    document.getElementById('contentDesc').innerHTML = "Both my Parents are from Lahore, Pakistan. Although I've never lived in Pakistan it still feels very familiar. It's quite important for me to stay connected with my roots.";
+}
+function cndContent() {
+    document.getElementById('contentDesc').innerHTML = "I'm Canadian-born but haven't experienced much of that land. I love visiting Canada for vacation as a good chunk of my family lives there. There's nothing better than eating some piping hot poutine in snowy weather";
+}
+function amrContent() {
+    document.getElementById('contentDesc').innerHTML = "Although my relationship with America is complex, I've spent almost my entire life in this country and consider it home. I spent my childhood in Redmond, Washington and have lived in a handful of states in the past.";
+}
+function socialContent() {
+    document.getElementById('contentDesc').innerHTML = "I'm passionate about helping out war-torn countries, dehumanized populations, pointing out the hypocrisy in justice and the imbalance of power on the international stage. It's a subject all too grandiose for an individual, but I hope to contribute in any way I can.";
+}
