d17bfcfbb938c9b8d0881e26bec9028d554060bd
Zeeshan Waqar <zeewaqar06@gmail.com>
2025-01-12 22:05:34 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -14,15 +14,16 @@ function closeNav() {
     navBar.style.zIndex = '-1';
 }
 
+
 function pkContent() {
-    document.getElementById('contentDesc').innerHTML = "Both my parents are from Lahore, Pakistan. Although I've never lived in Pakistan, every time I visit it feels like home. It's quite important for me to stay connected with my roots.";
+    document.getElementById('contentDesc').innerHTML = "Both my parents are from Lahore, Pakistan. Although I've never lived in Pakistan, every time I visit it feels like home; as if I never left in the first place. Naturally, I think it's important for me to stay connected with my roots and culture.";
 }
 function cndContent() {
-    document.getElementById('contentDesc').innerHTML = "I'm Canadian-born but I only visit the country during vacations since a lot of my family lives there. I enjoy snowy weather, eating poutine, and sight-seeing around the unique terrain.";
+    document.getElementById('contentDesc').innerHTML = "I'm a Canadian-born citizen but I live in Georgia. We often go up north for vacations since a lot of my family lives in Canada. I enjoy the snowy weather, eating Tim Hortons, and sight-seeing around the country.";
 }
 function amrContent() {
     document.getElementById('contentDesc').innerHTML = "Although my relationship with America is complex, I've spent almost my entire life in this country and consider it home. I spent my childhood in Redmond, Washington and have lived in a handful of states in the past.";
 }
 function socialContent() {
-    document.getElementById('contentDesc').innerHTML = "I'm passionate about helping out war-torn countries, dehumanized populations, pointing out the hypocrisy in justice and the imbalance of power on the international stage. It's a subject all too grandiose for an individual, but I hope to contribute in any way I can.";
+    document.getElementById('contentDesc').innerHTML = "I'm passionate about helping out war-torn countries, dehumanized populations, and pointing out the hypocrisy in justice/imbalance of power on the international stage. It's a subject all too grandiose for an individual like me, but I look to contribute in any way I can.";
 }
