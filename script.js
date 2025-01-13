4f9d4abcc142bab589b20b01e95518184f812aad
Zeeshan Waqar <zeewaqar06@gmail.com>
2025-01-12 19:47:57 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function pkContent() {
     document.getElementById('contentDesc').innerHTML = "Both my parents are from Lahore, Pakistan. Although I've never lived in Pakistan, every time I visit it feels like home. It's quite important for me to stay connected with my roots.";
 }
 function cndContent() {
-    document.getElementById('contentDesc').innerHTML = "I'm Canadian-born but I only visit the country during vacations since a lot of my family lives there. I enjoy snowy weather, eating poutine, and go sight-seeing around the unique terrain.";
+    document.getElementById('contentDesc').innerHTML = "I'm Canadian-born but I only visit the country during vacations since a lot of my family lives there. I enjoy snowy weather, eating poutine, and sight-seeing around the unique terrain.";
 }
 function amrContent() {
     document.getElementById('contentDesc').innerHTML = "Although my relationship with America is complex, I've spent almost my entire life in this country and consider it home. I spent my childhood in Redmond, Washington and have lived in a handful of states in the past.";
