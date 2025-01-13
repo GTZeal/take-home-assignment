23425524970486fbb5e237183cc4479690d08d9c
Zeeshan Waqar <zeewaqar06@gmail.com>
2025-01-12 19:02:36 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -15,10 +15,10 @@ function closeNav() {
 }
 
 function pkContent() {
-    document.getElementById('contentDesc').innerHTML = "Both my Parents are from Lahore, Pakistan. Although I've never lived in Pakistan, every time I visit it feels like home. It's quite important for me to stay connected with my roots.";
+    document.getElementById('contentDesc').innerHTML = "Both my parents are from Lahore, Pakistan. Although I've never lived in Pakistan, every time I visit it feels like home. It's quite important for me to stay connected with my roots.";
 }
 function cndContent() {
-    document.getElementById('contentDesc').innerHTML = "I'm Canadian-born but I only visit the country during vacations since a lot of my family lives there. I love visiting Canada for vacation as a good chunk of my family lives there. There's nothing better than eating some piping hot poutine in snowy weather";
+    document.getElementById('contentDesc').innerHTML = "I'm Canadian-born but I only visit the country during vacations since a lot of my family lives there. I enjoy snowy weather, eating poutine, and go sight-seeing around the unique terrain.";
 }
 function amrContent() {
     document.getElementById('contentDesc').innerHTML = "Although my relationship with America is complex, I've spent almost my entire life in this country and consider it home. I spent my childhood in Redmond, Washington and have lived in a handful of states in the past.";
